eddfc779be52064efbc3006786d4dded6bbb1634
Shanika Kalpanee Ambegoda <skalpanee6@gmail.com>
2026-03-20 22:40:12 +0530

    Update Server js file

--- gateway/server.js
+++ gateway/server.js
@@ -1,9 +1,12 @@
 const express = require("express");
+const cors = require('cors');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
+
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("Gateway service is running successfully!");
